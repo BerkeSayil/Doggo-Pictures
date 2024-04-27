6a6a92f433f878db77aca4ac6837881487934373
Berke Sayıl <berkesayil@gmail.com>
2024-04-27 20:09:25 +0300

    Fixed .mp4 issue

--- doggoGenerator.js
+++ doggoGenerator.js
@@ -6,11 +6,15 @@ async function getPic() {
   const promise = await fetch(DOG_URL);
   console.log(promise);
   const processedResponse = await promise.json();
+  if (processedResponse.url.endsWith('.mp4')) {
+    getPic();
+    return;
+  }
   const img = document.createElement("img");
   img.src = processedResponse.url;
   img.alt = "Cute DOGGO!";
   img.style.width = "300px";
-  img.style.height = "200px";
+  img.style.height = "300px";
   img.style.objectFit = "cover";
   dogg.appendChild(img);
 }
